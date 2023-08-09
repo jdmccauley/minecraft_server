6f285dd88c79aa3d364bb1c1202a1b59dc95279d
Joshua McCauley <jdm.mccauley@gmail.com>
2023-08-08 21:33:04 -0700

    Fix zip directory name
    Zip stored directory was being named with the timestamp instead of the
    expected "JAVA_SERVER", so that was changed.

--- sync.sh
+++ sync.sh
@@ -2,7 +2,9 @@
 
 pushd /root/OneDrive/Documents/Gaming/minecraft/server_backups > /dev/null
 
-zip -r "${1}.zip" "${1}"
+backup="$(date +%Y%m%d%H%M)_java_server_backup.zip"
+
+zip -r "${backup}" "${1}"
 rm -r "${1}"
 
 onedrive --synchronize --upload-only
